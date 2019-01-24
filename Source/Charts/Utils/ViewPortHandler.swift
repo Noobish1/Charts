e1b2d821ba346a92ed44b2b6f6f00a39cc4514b1
Blair McArthur <blair.mcarthur@skipthedishes.com>
2019-01-24 17:41:47 +1300

    added a debug print

--- Source/Charts/Utils/ViewPortHandler.swift
+++ Source/Charts/Utils/ViewPortHandler.swift
@@ -39,6 +39,8 @@ open class ViewPortHandler {
         _chartHeight = height
         _chartWidth = width
         
+        print("height: \(height), width: \(width), offsets: \(offsetTop), \(offsetLeft), \(offsetBottom), offsetRight)")
+        
         restrainViewPort(offsetLeft: offsetLeft, offsetTop: offsetTop, offsetRight: offsetRight, offsetBottom: offsetBottom)
     }
 
